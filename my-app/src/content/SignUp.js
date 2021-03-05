24d1d5d95be1b42a0caebde8acb24481f4a15830
eethomas14 <ethomas14@luc.edu>
2021-03-05 14:24:05 -0600

    more work on signup form validation, adding a confirm password field + validation

--- my-app/src/content/SignUp.js
+++ my-app/src/content/SignUp.js
@@ -23,11 +23,13 @@ class SignUp extends Component {
             lastName: null,
             email: null,
             password: null,
+            password2: null,
             errors: {
                 firstName: '',
                 lastName:'',
                 email: '',
                 password: '',
+                password2:''
             }
         };
     }
@@ -62,6 +64,13 @@ class SignUp extends Component {
                         ? 'Your password must be at least 6 characters long!'
                         : '';
                 break;
+            case 'password2':
+                errors.password2 =
+                    value !== document.getElementsByName("password")[0].value
+                        ? 'Your passwords do not match!'
+                        : '';
+                console.log(value.password)
+                break;
             default:
                 break;
         }
@@ -83,30 +92,32 @@ class SignUp extends Component {
 
             <div className="formContainer">
 
-                <form className="sign-up_form" onSubmit={this.handleSubmit} >
+                <form className="sign-up_form" onSubmit={this.handleSubmit} noValidate>
                     <div className="text-container">
                         <div className="form-input">
-                            <input className="input-field" type="text" placeholder="first name" name="firstName" onChange={this.handleChange} />
+                            <input className="input-field" type="text" placeholder="first name" name="firstName" onChange={this.handleChange} formNoValidate/>
                             {errors.firstName.length > 0 &&
                             <span className='error'>{errors.firstName}</span>}
                         </div>
 
                         <div className="form-input">
-                            <input className="input-field" type="text" placeholder="last name" name="lastName" onChange={this.handleChange} />
+                            <input className="input-field" type="text" placeholder="last name" name="lastName" onChange={this.handleChange} formNoValidate/>
                             {errors.lastName.length > 0 && <span className='error'>{errors.lastName}</span>}
                         </div>
 
                         <div className="form-input">
-                            <input className="input-field"  type="text" placeholder="email address" name="email" onChange={this.handleChange} />
+                            <input className="input-field"  type="text" placeholder="email address" name="email" onChange={this.handleChange} formNoValidate/>
                             {errors.email.length > 0 && <span className='error'>{errors.email}</span>}
                         </div>
 
                         <div className="form-input">
-                            <div className='info'>
 
-                            </div>
-                            <input className="input-field" type="password" placeholder="password" name="password" onChange={this.handleChange} />
+                            <input className="input-field" type="password" placeholder="password" name="password" onChange={this.handleChange} formNoValidate/>
                             {errors.password.length > 0 && <span className='error'>{errors.password}</span>}
+                        </div>
+                        <div className="form-input">
+                            <input className="input-field" type="password" placeholder="confirm password" name="password2" onChange={this.handleChange} formNoValidate/>
+                            {errors.password2.length > 0 && <span className='error'>{errors.password2}</span>}
                         </div>
 
                         <button className="btn--primary--form btn--small" type="submit">Create Account</button>
